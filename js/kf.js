827ef8729ccada99ffa4cd66735972768f430c77
zhaolinxu <zhaolinxu@sina.cn>
2018-12-12 18:43:37 +0800

    http协议自适应

--- js/kf.js
+++ js/kf.js
@@ -7,7 +7,7 @@ $(function(){
     content +='<div id="close_im" class="close-im"><a href="javascript:void(0);" title="点击关闭">&nbsp;</a></div>';
     content +='<a href="http://shang.qq.com/wpa/qunwpa?idkey=d10d784492d5e73cca25ff48f4763bdc14f698ffdbf42adbb0e9304495cb0387" target="_blank" class="im-qq qq-a" title="点击加入 - GitHub游戏交流群:627141737">';
     content +='<div class="qq-container"></div>';
-    content +='<div class="qq-hover-c"><img class="img-qq" src="http://likexia.gitee.io/game/images/kf/qq.png"></div>';
+    content +='<div class="qq-hover-c"><img class="img-qq" src="//likexia.gitee.io/game/images/kf/qq.png"></div>';
     content +='<span>点击加群</span>';
     content +='</a>';
     content +='<div class="im-tel">';
@@ -20,7 +20,7 @@ $(function(){
     content +='<div class="weixing-container">';
     content +='<div class="weixing-show">';
     content +='<div class="weixing-txt" style="font-size:13px;">支付宝扫一扫<br>每天领红包<br>0.1 - 99元<br><a href="http://likexia.gitee.io/game/article/fuli.html" target="_blank" title="凡是扫这个二维码领的红包，都可以找我拿返现，点击查看详情">查看详情</a></div>';
-    content +='<img class="weixing-ma" src="http://likexia.gitee.io/game/images/kf/zfb-ma.jpg">';
+    content +='<img class="weixing-ma" src="//likexia.gitee.io/game/images/kf/zfb-ma.jpg">';
     content +='<div class="weixing-sanjiao"></div>';
     content +='<div class="weixing-sanjiao-big"></div>';
     content +='</div>';

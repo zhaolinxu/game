22355d82a5b681034f6660acff1d8f0de539bd21
zhaolinxu <zhaolinxu@sina.cn>
2019-07-25 11:34:56 +0800

    汉化

---
+++ zh/eternitytower-auto.js
@@ -0,0 +1,16 @@
+/*
+
+ @name    : 锅巴汉化 - 自动插件
+ @author  : 好阳光的小锅巴
+ @version : V0.1.1 - 2019-07-25
+ @website : http://www.g8hh.com
+
+*/
+
+
+
+! function () {
+    console.log("加载自动化脚本");
+
+
+}();

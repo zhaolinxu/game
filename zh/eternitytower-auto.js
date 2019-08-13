442c3c0a9a2cd982282ffe68f6b678843f5f2dbc
zhaolinxu <zhaolinxu@sina.cn>
2019-08-13 10:34:33 +0800

    陨石精华

--- zh/eternitytower-auto.js
+++ zh/eternitytower-auto.js
@@ -195,6 +195,7 @@
     content += '<option value="cobaltEssence">钴精华</option>';
     content += '<option value="mithrilEssence">秘银精华</option>';
     content += '<option value="adamantiumEssence">埃德曼合金精华</option>';
+    content += '<option value="meteoriteEssence">陨石精华</option>';
     content += '</select> ';
     content += '挖矿间隔 <input id="minTime" type="text" value="300" placeholder="输入整数数字" autocomplete="on"/> 秒；';
     content += '<br/>';

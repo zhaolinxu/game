3ceb159c5fe863d6f0ca5d7d6382a43019f99c23
zhaolinxu <zhaolinxu@sina.cn>
2021-04-07 15:30:39 +0800

    修改群链接

--- static/js/kf.js
+++ static/js/kf.js
@@ -22,7 +22,7 @@ $(function() {
   // content +='<div class="weixing-txt" style="font-size:13px;">支付宝扫一扫<br>每天领红包<br>0.1 - 99元<br><a href="http://likexia.gitee.io/game/article/fuli.html" target="_blank" title="凡是扫这个二维码领的红包，都可以找我拿返现，点击查看详情">查看详情</a></div>';
   // content +='<img class="weixing-ma" src="//likexia.gitee.io/game/images/kf/zfb-ma.jpg">';
   //    content +='<div class="weixing-txt" style="font-size:13px;">打开手机QQ<br>扫码加群<br>一起讨论吧<br><a href="http://shang.qq.com/wpa/qunwpa?idkey=d10d784492d5e73cca25ff48f4763bdc14f698ffdbf42adbb0e9304495cb0387" target="_blank" title="点击加入 - GitHub游戏交流群:627141737">点击加群</a></div>';
-  content += '<div class="weixing-txt" style="font-size:13px;">打开微信扫一扫<br>关注公众号<br><br><a href="http://shang.qq.com/wpa/qunwpa?idkey=d10d784492d5e73cca25ff48f4763bdc14f698ffdbf42adbb0e9304495cb0387" target="_blank" title="点击加入 - Git游戏交流群:627141737">Git游戏</a></div>'
+  content += '<div class="weixing-txt" style="font-size:13px;">打开微信扫一扫<br>关注公众号<br><br><a href="//qm.qq.com/cgi-bin/qm/qr?k=AnZJbnsU8BsmQGGEWs4rXd-JuSJI-j9c&jump_from=webapi" target="_blank" title="点击加入 - Git游戏放置交流群:793344327">Git游戏</a></div>'
   content += '<img class="weixing-ma" src="http://likexia.gitee.io/game/images/kf/weixing-ma.jpg">'
   content += '<div class="weixing-sanjiao"></div>'
   content += '<div class="weixing-sanjiao-big"></div>'

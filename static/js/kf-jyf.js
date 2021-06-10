9c813a8c14a81d68786220ab8df99d635bbfad26
zhaolinxu <zhaolinxu@sina.cn>
2021-06-10 09:56:16 +0800

    更新

--- static/js/kf-jyf.js
+++ static/js/kf-jyf.js
@@ -7,23 +7,23 @@ $(function(){
     content +='<div id="close_im" class="close-im"><a href="javascript:void(0);" title="点击关闭">&nbsp;</a></div>';
     content +='<a href="https://qm.qq.com/cgi-bin/qm/qr?k=AnZJbnsU8BsmQGGEWs4rXd-JuSJI-j9c&jump_from=webapi" target="_blank" class="im-qq qq-a" title="点击加入 - Git游戏交流群:793344327">';
     content +='<div class="qq-container"></div>';
-    content +='<div class="qq-hover-c"><img class="img-qq" src="//g8hh.com/static/images/kf/qq.png"></div>';
+    content +='<div class="qq-hover-c"><img class="img-qq" src="//likexia.gitee.io/game/images/kf/qq.png"></div>';
     content +='<span>点击加群</span>';
     content +='</a>';
     content +='<div class="im-tel">';
-    content +='<div>空灵农场QQ群:</div>';
-    content +='<div class="tel-num">946050761</div>';
+    content +='<div>QQ群号:</div>';
+    content +='<div class="tel-num">922600429</div>';
     content +='<div>更多同类游戏:</div>';
     content +='<div class="tel-num"><a href="http://www.gityx.com" target="_blank" title="Git游戏：gityx.com~">Git游戏</a><a href="http://g8hh.com" target="_blank" style="margin-left:5px;" title="锅巴汉化：g8hh.com">锅巴汉化</a><br /><a href="http://bbs.gityx.com/" target="_blank" style="margin-left:5px;" title="点击查看游戏攻略">游戏攻略交流论坛</a></div>';
     content +='</div>';
     content +='<div class="im-footer" style="position:relative">';
     content +='<div class="weixing-container">';
     content +='<div class="weixing-show">';
-    //content +='<div class="weixing-txt" style="font-size:13px;">支付宝扫一扫<br>每天领红包<br>0.1 - 99元<br><a href="http://g8hh.com/static/article/fuli.html" target="_blank" title="凡是扫这个二维码领的红包，都可以找我拿返现，点击查看详情">查看详情</a></div>';
-    //content +='<img class="weixing-ma" src="//g8hh.com/static/images/kf/zfb-ma.jpg">';
+    //content +='<div class="weixing-txt" style="font-size:13px;">支付宝扫一扫<br>每天领红包<br>0.1 - 99元<br><a href="http://likexia.gitee.io/game/article/fuli.html" target="_blank" title="凡是扫这个二维码领的红包，都可以找我拿返现，点击查看详情">查看详情</a></div>';
+    //content +='<img class="weixing-ma" src="//likexia.gitee.io/game/images/kf/zfb-ma.jpg">';
 //    content +='<div class="weixing-txt" style="font-size:13px;">打开手机QQ<br>扫码加群<br>一起讨论吧<br><a href="http://shang.qq.com/wpa/qunwpa?idkey=d10d784492d5e73cca25ff48f4763bdc14f698ffdbf42adbb0e9304495cb0387" target="_blank" title="点击加入 - GitHub游戏交流群:627141737">点击加群</a></div>';
     content +='<div class="weixing-txt" style="font-size:13px;">打开微信扫一扫<br>关注公众号<br><br><a href="http://shang.qq.com/wpa/qunwpa?idkey=d10d784492d5e73cca25ff48f4763bdc14f698ffdbf42adbb0e9304495cb0387" target="_blank" title="点击加入 - Git游戏交流群:627141737">Git游戏</a></div>';
-    content +='<img class="weixing-ma" src="http://g8hh.com/static/images/kf/weixing-ma.jpg">';
+    content +='<img class="weixing-ma" src="http://likexia.gitee.io/game/images/kf/weixing-ma.jpg">';
     content +='<div class="weixing-sanjiao"></div>';
     content +='<div class="weixing-sanjiao-big"></div>';
     content +='</div>';
@@ -59,7 +59,7 @@ $(function(){
   setTimeout(function () {
     $('#im_main').hide();
   },5000);
-  //    关闭小球，防止遮挡
+      //    关闭小球，防止遮挡
   $('.close-ball').bind('click', function() {
     if ($('.close-ball').hasClass('show')) {
       $('#open_im').show()
--- static/js/kf-pokeclicker.js
+++ static/js/kf-pokeclicker.js
@@ -5,16 +5,16 @@ $(function(){
     content +='<div id="open_im" class="open-im">&nbsp;</div><div class="close-ball">x</div>';
     content +='<div class="im_main" id="im_main">';
     content +='<div id="close_im" class="close-im"><a href="javascript:void(0);" title="点击关闭">&nbsp;</a></div>';
-    content +='<a href="https://qm.qq.com/cgi-bin/qm/qr?k=AnZJbnsU8BsmQGGEWs4rXd-JuSJI-j9c&jump_from=webapi" target="_blank" class="im-qq qq-a" title="点击加入 - Git游戏交流群:793344327">';
+    content +='<a href="https://qm.qq.com/cgi-bin/qm/qr?k=I_MDtJGuVznxr0dI5drF_ByolzWHch9W&jump_from=webapi"" target="_blank" class="im-qq qq-a" title="点击加入 - 宝可梦点击交流群:595329229">';
     content +='<div class="qq-container"></div>';
     content +='<div class="qq-hover-c"><img class="img-qq" src="//g8hh.com/static/images/kf/qq.png"></div>';
     content +='<span>点击加群</span>';
     content +='</a>';
     content +='<div class="im-tel">';
-    content +='<div>空灵农场QQ群:</div>';
-    content +='<div class="tel-num">946050761</div>';
+    content +='<div>QQ群号:</div>';
+    content +='<div class="tel-num">595329229</div>';
     content +='<div>更多同类游戏:</div>';
-    content +='<div class="tel-num"><a href="http://www.gityx.com" target="_blank" title="Git游戏：gityx.com~">Git游戏</a><a href="http://g8hh.com" target="_blank" style="margin-left:5px;" title="锅巴汉化：g8hh.com">锅巴汉化</a><br /><a href="http://bbs.gityx.com/" target="_blank" style="margin-left:5px;" title="点击查看游戏攻略">游戏攻略交流论坛</a></div>';
+    content +='<div class="tel-num"><a href="http://www.gityx.com" target="_blank" title="Git游戏：gityx.com~">Git游戏</a><a href="http://g8hh.com" target="_blank" style="margin-left:5px;" title="锅巴汉化：g8hh.com">锅巴汉化</a><br /><a href="http://bbs.gityx.com/" target="_blank" style="margin-left:5px;" title="点击查看游戏攻略">游戏攻略交流论坛</a><br /></div>';
     content +='</div>';
     content +='<div class="im-footer" style="position:relative">';
     content +='<div class="weixing-container">';
@@ -22,7 +22,7 @@ $(function(){
     //content +='<div class="weixing-txt" style="font-size:13px;">支付宝扫一扫<br>每天领红包<br>0.1 - 99元<br><a href="http://g8hh.com/static/article/fuli.html" target="_blank" title="凡是扫这个二维码领的红包，都可以找我拿返现，点击查看详情">查看详情</a></div>';
     //content +='<img class="weixing-ma" src="//g8hh.com/static/images/kf/zfb-ma.jpg">';
 //    content +='<div class="weixing-txt" style="font-size:13px;">打开手机QQ<br>扫码加群<br>一起讨论吧<br><a href="http://shang.qq.com/wpa/qunwpa?idkey=d10d784492d5e73cca25ff48f4763bdc14f698ffdbf42adbb0e9304495cb0387" target="_blank" title="点击加入 - GitHub游戏交流群:627141737">点击加群</a></div>';
-    content +='<div class="weixing-txt" style="font-size:13px;">打开微信扫一扫<br>关注公众号<br><br><a href="http://shang.qq.com/wpa/qunwpa?idkey=d10d784492d5e73cca25ff48f4763bdc14f698ffdbf42adbb0e9304495cb0387" target="_blank" title="点击加入 - Git游戏交流群:627141737">Git游戏</a></div>';
+    content +='<div class="weixing-txt" style="font-size:13px;">打开微信扫一扫<br>关注公众号<br><br><a href="https://mp.weixin.qq.com/s/ezp5x6hOC8QPLnZ2bPFyEw" target="_blank" title="欢迎关注Git游戏关注，每周都有新游戏！">Git游戏</a></div>';
     content +='<img class="weixing-ma" src="http://g8hh.com/static/images/kf/weixing-ma.jpg">';
     content +='<div class="weixing-sanjiao"></div>';
     content +='<div class="weixing-sanjiao-big"></div>';
@@ -59,7 +59,7 @@ $(function(){
   setTimeout(function () {
     $('#im_main').hide();
   },5000);
-  //    关闭小球，防止遮挡
+      //    关闭小球，防止遮挡
   $('.close-ball').bind('click', function() {
     if ($('.close-ball').hasClass('show')) {
       $('#open_im').show()
